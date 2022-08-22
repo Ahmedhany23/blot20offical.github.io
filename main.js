d2f3108232e8d0ee2b63a3b584a4ae223d392b86
Ahmed HanyELD <ahmedhany.warframe@gmail.com>
2022-08-22 08:14:34 +0200

    added

---
+++ main.js
@@ -0,0 +1,5 @@
+var links = document.getElementById("icon-logo");
+links.onclick = function() { if (document.querySelector(".header .links ul").style.display === "none"){
+    document.querySelector(".header .links ul").style.display = "block";
+} 
+else{document.querySelector(".header .links ul").style.display = "none"}};
