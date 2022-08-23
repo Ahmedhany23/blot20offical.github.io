d6b4a118b4a6c16a82d99bb77d4803a23dd37396
Ahmed HanyELD <89325347+BLOT20@users.noreply.github.com>
2022-08-23 09:21:10 +0200

    Update main.js

--- Leon/js/main.js
+++ Leon/js/main.js
@@ -4,5 +4,3 @@ links.onclick = function() { if (document.querySelector(".header .links ul").sty
 } 
 else{document.querySelector(".header .links ul").style.display = "none"}};
 
-var clickhere = document.getElementById("portfolio-content");
-clickhere.onclick = function(){location.href = "#"};
