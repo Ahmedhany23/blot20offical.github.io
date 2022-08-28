fb87c1c23a0ae11fa32d157fbeec59f7ed8fef1c
Ahmed HanyELD <89325347+BLOT20@users.noreply.github.com>
2022-08-28 14:58:45 +0200

    Update main.js

--- Leon/js/main.js
+++ Leon/js/main.js
@@ -4,3 +4,21 @@ links.onclick = function() { if (document.querySelector(".header .links ul").sty
 } 
 else{document.querySelector(".header .links ul").style.display = "none"}};
 
+var links = document.getElementById("icon-logo");
+links.onclick = function() { if (document.querySelector(".header .links ul").style.display === "none"){
+    document.querySelector(".header .links ul").style.display = "block";
+} 
+else{document.querySelector(".header .links ul").style.display = "none"}};
+
+var btn = document.querySelector(".up");
+
+window.onscroll = function(){
+    if(window.scrollY >= 600) {
+        btn.style.display = "inline";
+        btn.style.right = "10px";
+    }
+    else {
+        btn.style.display = "none";
+    }
+}
+btn.onclick = function() {window.scroll(0,0)}
